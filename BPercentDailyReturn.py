1b827e7c0a9115818348c8c4c9b119b9699729ad
LeoZ06 <79598689+LeoZ06@users.noreply.github.com>
2022-03-04 17:35:28 +0000

    Worked on B.

--- BPercentDailyReturn.py
+++ BPercentDailyReturn.py
@@ -7,5 +7,9 @@ Note: the percent daily return = 100 * (current day - previous day)/previous day
 
 Below you are given the stock prices (there are only 4 days in this example). First convert to a numpy array. Then use slicing and numeric operations to calculate the percent daily return (no for loops!).
 """
-
+import numpy as np
 acme = [10, 11.5, 11, 10, 12]
+prices = np.array(acme)
+current_price = prices[1: 5]
+previous_price = prices[0: 4]
+difference = current_price - previous_price
